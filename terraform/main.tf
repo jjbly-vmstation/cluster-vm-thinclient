59cb6b491e880602916b32edc56c5a06f2678d7c
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2026-02-26 20:55:13 -0500

    deepseek time

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,73 +12,78 @@ provider "libvirt" {
   uri = var.libvirt_uri
 }
 
-# --- Storage ---
+# ----------------------------------------------------------------------
+# Volumes
+# ----------------------------------------------------------------------
 resource "libvirt_volume" "windows_os" {
-  name   = "windows-os.qcow2"
-  pool   = var.pool_name
-  size   = var.os_disk_size_gb * 1024 * 1024 * 1024
-  format = "qcow2"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+  name = "${var.vm_name}-os.qcow2"
+  pool = var.pool_name
+  size = var.os_disk_size_gb * 1024 * 1024 * 1024
 }
 
 resource "libvirt_volume" "windows_data" {
-  name   = "windows-data.qcow2"
-  pool   = var.pool_name
-  size   = var.data_disk_size_gb * 1024 * 1024 * 1024
-  format = "qcow2"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+  name = "${var.vm_name}-data.qcow2"
+  pool = var.pool_name
+  size = var.data_disk_size_gb * 1024 * 1024 * 1024
 }
 
-# --- Domain Definition ---
+# ----------------------------------------------------------------------
+# Domain (VM)
+# ----------------------------------------------------------------------
 resource "libvirt_domain" "windows" {
-  name     = var.vm_name
-  memory   = var.memory_mb
-  vcpu     = var.vcpus
-  type     = "kvm"
-  machine  = "q35" # Required for modern Windows 11
-  firmware = var.firmware_path
+  name      = var.vm_name
+  machine   = "q35"                     # Windows 11 requires Q35
+  firmware  = var.firmware_path          # OVMF with Secure Boot support
+  autostart = true
+
+  vcpu   = var.vcpus
+  memory = var.memory_mb
 
   cpu {
     mode = "host-passthrough"
   }
 
-  network_interface {
-    network_name = var.network_name
-    mac          = var.mac_address
-  }
-
-  # Disk 1: Installer ISO
-  disk {
-    file = "/home/vmadmin/iso/en-us_windows_11_business_editions_version_25h2_updated_feb_2026_x64_dvd_9271bf68.iso"
-  }
-
-  # Disk 2: OS Disk
-  disk {
-    volume_id = libvirt_volume.windows_os.id
-  }
-
-  # Disk 3: VirtIO Drivers
-  disk {
-    file = "/home/vmadmin/iso/virtio-win.iso"
-  }
-
-  nvram {
-    file     = "/home/vmadmin/disks/windows-vars.fd"
-    template = var.nvram_template
-  }
-
-  # Windows 11 Enterprise requires TPM 2.0
+  # TPM 2.0 (emulated)
   tpm {
     backend_type    = "emulator"
     backend_version = "2.0"
   }
 
+  # Network (using your pinned MAC)
+  network_interface {
+    network_name = var.network_name
+    mac          = var.mac_address
+  }
+
+  # Disk 1: Windows installation ISO
+  disk {
+    file = var.iso_path
+  }
+
+  # Disk 2: OS disk – attached as SATA (no drivers needed during install)
+  disk {
+    volume_id  = libvirt_volume.windows_os.id
+    target_bus = "sata"
+  }
+
+  # Disk 3: Data disk – also SATA
+  disk {
+    volume_id  = libvirt_volume.windows_data.id
+    target_bus = "sata"
+  }
+
+  # Disk 4: VirtIO drivers ISO (for virtio-gpu, virtio-net, etc.)
+  disk {
+    file = var.virtio_iso_path
+  }
+
+  # UEFI NVRAM – a copy of the template is created for this VM
+  nvram {
+    file     = var.nvram_file
+    template = var.nvram_template
+  }
+
+  # Graphics (VNC)
   graphics {
     type           = "vnc"
     listen_type    = "address"
@@ -86,21 +91,26 @@ resource "libvirt_domain" "windows" {
     autoport       = true
   }
 
+  # Video – virtio (drivers will be installed from the virtio‑win ISO)
   video {
-    type = "vga" # Use VGA for initial installation to avoid driver hang
+    type = "virtio"
   }
 
-  # Hardware Patching (Robustness Layer)
+  # --------------------------------------------------------------------
+  # XML patching (XSLT) – adds SMM, secure loader flag, boot order
+  # --------------------------------------------------------------------
   xml {
     xslt = <<EOF
 <?xml version="1.0" ?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
   <xsl:output omit-xml-declaration="yes" indent="yes"/>
 
+  <!-- Identity transform -->
   <xsl:template match="node()|@*">
     <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>
   </xsl:template>
 
+  <!-- Add SMM feature (required for Secure Boot + TPM) -->
   <xsl:template match="/domain/features">
     <xsl:copy>
       <xsl:apply-templates select="node()|@*"/>
@@ -108,6 +118,7 @@ resource "libvirt_domain" "windows" {
     </xsl:copy>
   </xsl:template>
 
+  <!-- Ensure loader uses pflash and secure='yes' -->
   <xsl:template match="/domain/os/loader">
     <xsl:copy>
       <xsl:apply-templates select="@*"/>
@@ -117,8 +128,7 @@ resource "libvirt_domain" "windows" {
     </xsl:copy>
   </xsl:template>
 
-  <xsl:template match="/domain/devices/disk/boot"/>
-
+  <!-- Set boot order: CDROM first, then hard disk, show boot menu -->
   <xsl:template match="/domain/os">
     <xsl:copy>
       <xsl:apply-templates select="node()|@*"/>
@@ -128,13 +138,8 @@ resource "libvirt_domain" "windows" {
     </xsl:copy>
   </xsl:template>
 
-  <xsl:template match="/domain/devices/disk/target">
-    <xsl:copy>
-      <xsl:apply-templates select="@*"/>
-      <xsl:attribute name="bus">sata</xsl:attribute>
-    </xsl:copy>
-  </xsl:template>
-
+  <!-- Remove stray <boot> elements under disks -->
+  <xsl:template match="/domain/devices/disk/boot"/>
 </xsl:stylesheet>
 EOF
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "libvirt_uri" {
-  description = "Libvirt connection URI (qemu+ssh for remote)"
+  description = "Libvirt connection URI (e.g., qemu:///system or qemu+ssh://...)"
   type        = string
   default     = "qemu:///system"
 }
@@ -19,13 +19,13 @@ variable "vm_name" {
 variable "memory_mb" {
   description = "VM memory in MB"
   type        = number
-  default     = 4096
+  default     = 12288
 }
 
 variable "vcpus" {
   description = "Number of vCPUs"
   type        = number
-  default     = 2
+  default     = 4
 }
 
 variable "os_disk_size_gb" {
@@ -41,13 +41,18 @@ variable "data_disk_size_gb" {
 }
 
 variable "iso_path" {
-  description = "Path to Windows install ISO (e.g. /home/vmadmin/iso/Win11.iso)"
+  description = "Full path to Windows 11 installation ISO"
   type        = string
-  default     = ""
+}
+
+variable "virtio_iso_path" {
+  description = "Full path to virtio-win ISO (drivers for virtio devices)"
+  type        = string
+  default     = "/home/vmadmin/iso/virtio-win.iso"   # matches your listing
 }
 
 variable "network_name" {
-  description = "Libvirt network name (default NAT) or bridge (e.g. br0)"
+  description = "Libvirt network name (default NAT) or bridge (e.g., br0)"
   type        = string
   default     = "default"
 }
@@ -55,17 +60,25 @@ variable "network_name" {
 variable "mac_address" {
   description = "Pinned MAC address for stable Windows activation"
   type        = string
-  default     = "52:54:00:a1:b2:c3"  # Replace with desired MAC
+  default     = "52:54:00:00:f0:0d"
 }
 
 variable "firmware_path" {
   description = "Path to OVMF Secure Boot firmware code"
   type        = string
-  default     = "/home/vmadmin/disks/firmware/OVMF_CODE.secboot.fd"
+  # Using the system path (you can override if you copied the file elsewhere)
+  default     = "/usr/share/edk2/ovmf/OVMF_CODE.secboot.fd"
 }
 
 variable "nvram_template" {
-  description = "Path to OVMF variables template"
+  description = "Path to OVMF variables template (with Secure Boot keys)"
   type        = string
-  default     = "/home/vmadmin/disks/firmware/OVMF_VARS.fd"
+  # System provides OVMF.qemuvars.fd as a suitable template
+  default     = "/usr/share/edk2/ovmf/OVMF.qemuvars.fd"
+}
+
+variable "nvram_file" {
+  description = "Path where the VM‑specific NVRAM copy will be stored"
+  type        = string
+  default     = "/home/vmadmin/disks/windows-vars.fd"
 }
