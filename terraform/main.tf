ab3675aecf94161f458d1b8ea1e95f13f2f7372c
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2026-03-02 14:49:32 -0500

    da saaa sa

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,21 +15,18 @@ provider "vmworkstation" {
   https    = false
 }
 
-resource "vmworkstation_virtual_machine" "windows_vm" {
+# Change from vmworkstation_virtual_machine to vmworkstation_vm
+resource "vmworkstation_vm" "windows_vm" {
   name          = var.vm_name
   os            = "windows11-64"
   memory        = var.memory_mb
   cpus          = var.vcpus
-  
-  # This version uses simpler paths
   path          = "/home/vmadmin/vmstation-org/cluster-vm-thinclient/terraform/${var.vm_name}.vmx"
 
-  # Networking
   network_adapter {
     type = "bridged"
   }
 
-  # ISO for Installation
   cdrom {
     path = var.iso_path
   }
