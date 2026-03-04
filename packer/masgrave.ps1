9e80ef3514cac36617d0440f216faad7ddf1dd4b
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2026-03-04 09:23:43 -0500

    corrected disk to chose install location

--- packer/masgrave.ps1
+++ packer/masgrave.ps1
@@ -1,5 +1,3 @@
-# masgrave.ps1
 Write-Output "Starting unattended Massgrave activation..."
-# The /HWID flag bypasses the menu and automatically activates Windows
-iex "& { $(irm https://massgrave.dev/get) } /HWID"
+& ([ScriptBlock]::Create((irm https://massgrave.dev/get))) /HWID
 Write-Output "Activation completed."
