dc0187332516a288aa508a37e2dee36cfa7f54df
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2026-03-04 21:54:00 -0500

    moved to FWLink for office deployment tool

--- packer/install_office.ps1
+++ packer/install_office.ps1
@@ -1,5 +1,8 @@
+$ErrorActionPreference = "Stop"
+
 Write-Output "Downloading Office Deployment Tool..."
-Invoke-WebRequest -Uri "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB369D707E/officedeploymenttool_17328-20162.exe" -OutFile "C:\Windows\Temp\odt.exe"
+# Using the permanent link to ensure we always get the latest version and avoid 404s
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/p/?LinkID=626065" -OutFile "C:\Windows\Temp\odt.exe"
 
 Write-Output "Extracting ODT..."
 Start-Process -FilePath "C:\Windows\Temp\odt.exe" -ArgumentList "/extract:C:\Windows\Temp\odt /quiet" -Wait
