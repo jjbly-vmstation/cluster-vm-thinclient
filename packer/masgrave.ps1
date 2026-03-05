7855c82501978b61a1011fb0200f32868f6eb46c
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2026-03-04 22:02:21 -0500

    removed silence on masgrave script

--- packer/masgrave.ps1
+++ packer/masgrave.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
 Write-Output "Starting unattended Massgrave activation..."
-& ([ScriptBlock]::Create((irm https://get.activated.win))) /HWID /Ohook /S
+& ([ScriptBlock]::Create((irm https://get.activated.win))) /HWID /Ohook
 if ($LASTEXITCODE -ne 0) { throw "Activation failed with exit code $LASTEXITCODE" }
 Write-Output "Activation completed."
