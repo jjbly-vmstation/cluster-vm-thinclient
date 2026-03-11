8224f425e3711f27b1f04547c2f7d6b62bf2976e
DilbagBains <47619061+DilbagBains@users.noreply.github.com>
2026-03-11 13:05:27 -0400

    a ds ad

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,11 +10,12 @@ terraform {
 provider "hyperv" {
   user     = var.hyperv_user
   password = var.hyperv_password
-  host     = var.hyperv_host
+  host     = "127.0.0.1"
   port     = 5985
   https    = false
   insecure = true
-  use_ntlm = true
+  use_ntlm = false
+  timeout  = "60s"
 }
 
 # Copy the golden VHDX from NFS to the F: RAID10 drive for this VM
